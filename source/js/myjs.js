6a3171e833d823a65cfb7f04480adcf2bd2e9654
Oleg Modenov <olegmodenov@gmail.com>
2019-09-22 23:52:12 +0500

    Сделаны модальные окна

--- source/js/myjs.js
+++ source/js/myjs.js
@@ -35,10 +35,20 @@ try {
   var close_success = popup_success.querySelector(".success__btn");
   var overlay = document.querySelector(".overlay");
   var form = document.querySelector(".form");
-  var person_name = form.querySelector(".person__name");
-  var surname = form.querySelector(".person__surname");
-  var phone = form.querySelector("[type=tel]");
-  var mail = form.querySelector("[name=mail]");
+  var fio__name = document.querySelector("[name=fio__name]");
+  var fio__surname = document.querySelector("[name=fio__surname]");
+
+  //Открытие модальных окон при отправке формы
+  form.addEventListener("submit", function (evt) {
+    overlay.classList.add("modal__show")
+    if (!fio__name.value || !fio__surname.value) {
+      evt.preventDefault();
+      popup_fail.classList.add("modal__show");
+    } else {
+      evt.preventDefault();
+      popup_success.classList.add("modal__show");
+    }
+  });
 
   // Закрытие модальных окон по кнопке
   close_fail.addEventListener("click", function (evt) {
@@ -60,23 +70,6 @@ try {
       popup_success.classList.remove("modal__show");
       popup_fail.classList.remove("modal__show");
       overlay.classList.remove("modal__show");
-    }
-  });
-
-  //Открытие модальных окон при отправке формы
-  form.addEventListener("submit", function (evt) {
-    overlay.classList.add("modal__show")
-    if (!person_name.value || !surname.value) {
-      evt.preventDefault();
-      popup_fail.classList.add("modal__show");
-      person_name.classList.add("field__text--error");
-      surname.classList.add("field__text--error");
-    }
-    else {
-      evt.preventDefault();
-      popup_success.classList.add("modal__show");
-      person_name.classList.remove("field__text--error");
-      surname.classList.remove("field__text--error");
     }
   });
 } catch (err) {
